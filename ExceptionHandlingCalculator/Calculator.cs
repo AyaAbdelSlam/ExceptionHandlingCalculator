9c6ab9b9feffd1663c0a1a30c73851ff6f5db21c
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2020-09-20 16:45:25 +0200

    Adding Calculator Class

---
+++ ExceptionHandlingCalculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandlingCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int num1, int num2, string operation)
+        {
+            if (operation == "/")
+            {
+                return this.Divide(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Unknown Operation");
+                return 0;
+            }
+        }
+
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
