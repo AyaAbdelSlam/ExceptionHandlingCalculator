bbfa46be6edcf09c215805d3b8cf469b5d923a44
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2020-09-26 21:22:52 +0200

    Adding Global unhandled Exception.

--- ExceptionHandlingCalculator/Program.cs
+++ ExceptionHandlingCalculator/Program.cs
@@ -7,7 +7,9 @@ namespace ExceptionHandlingCalculator
         static void Main(string[] args)
         {
             var calculator = new Calculator();
-            
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
+
             Console.WriteLine("Please enter the first number.");
             int number1 = int.Parse(Console.ReadLine());
 
@@ -56,6 +58,9 @@ namespace ExceptionHandlingCalculator
             }
         }
 
-
+        public static void HandleException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine($"Sorry there was a problem and we need to close. Details {e.ExceptionObject}");
+        }
     }
 }
